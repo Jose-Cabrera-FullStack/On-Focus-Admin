94818da9a2ea763cf9d5b5469d1f1ecf73daaddc
Luis Garces <luisgarcesleon@gmail.com>
2020-07-13 16:58:37 -0300

    fixes for heroku build

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].bundle.js",
-    publicPath: "./",
+    publicPath: "/",
     // publicPath: '/' Para Development
     // publicPath: './' Para produccion
   },
