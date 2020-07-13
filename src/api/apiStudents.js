82b64a2bb745081db32d4b945da4e14653c7284a
Luis Garces <luisgarcesleon@gmail.com>
2020-07-13 10:22:24 -0300

    fixes and implementations

---
+++ src/api/apiStudents.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL: 'http://localhost:4000/api',
+})
+
+// Students Endpoints
+export const insertStudent = payload => api.post(`/student`, payload)
+export const getAllStudents = () => api.get(`/student`)
+export const updateStudentById = (id, payload) => api.put(`/student/${id}`, payload)
+export const deleteStudentById = id => api.delete(`/student/${id}`)
+export const getStudentById = id => api.get(`/student/${id}`)
+
+const apiStudents = {
+    insertStudent,
+    getAllStudents,
+    updateStudentById,
+    deleteStudentById,
+    getStudentById,
+}
+
+export default apiStudents
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+
+// required libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
 
